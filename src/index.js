972c202b456b0b6a2d1d7270362a05a3212d4d2f
innei <tukon479@gmail.com>
2019-05-28 19:52:05 +0800

    ver3

--- src/index.js
+++ src/index.js
@@ -1,28 +1,76 @@
 window.onload = function () {
-   
+
+    completed();
+    const theme = document.getElementById('changeTheme');
+    const font = document.getElementById('changeFont');
+    theme.onclick = changeTheme;
+    font.onclick = changeFont;
+}
+
+function completed() {
     document.body.classList.add('completed');
     setTimeout(() => {
-        document.body.removeAttribute('class')
+        document.body.removeAttribute('class');
     }, 500);
-    const btn = document.getElementById('changeTheme');
-    btn.onclick = changeTheme;
 }
 
 function changeTheme() {
     const doc = document.documentElement;
     const body = document.body;
     if (doc.classList.contains('white')) {
-       body.classList.add('loading');
+        body.classList.add('loading');
         setTimeout(() => {
-            document.body.classList.remove('loading');
-        },500)
+            body.classList.add('completed');
+            setTimeout(() => {
+                body.removeAttribute('class')
+            }, 500);
+        }, 500)
         doc.classList.remove('white')
     }
     else {
         body.classList.add('loading');
+        body.classList.add('loading');
         setTimeout(() => {
-            document.body.classList.remove('loading');
+            body.classList.add('completed');
+            setTimeout(() => {
+                body.removeAttribute('class')
+            }, 500);
         }, 500)
         doc.classList.add('white')
     }
 }
+var f;
+function changeFont() {
+    const head = document.head;
+    const font = document.createElement('link');
+    const doc = document.documentElement;
+    if (!f) {
+        font.setAttribute('rel', 'stylesheet');
+        font.setAttribute('href', './src/font.css');
+        head.appendChild(font);
+        f = true;
+    }
+    if (!doc.classList.contains('font')) {
+        doc.classList.add('font')
+    }
+    else {
+        doc.classList.remove('font')
+    }
+
+}
+
+// pjax 
+
+new Pjax({
+    elements: "a:not([target])",
+    selectors: [
+        "title",
+        "main",
+        'nav',
+    ],
+    cacheBust: false,
+})
+document.addEventListener('pjax:send', () => {
+    document.body.classList.add("loading")
+})
+document.addEventListener('pjax:complete', window.onload);
